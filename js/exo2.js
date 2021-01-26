6d09566c539e34dae0e46cbaf8ab2c739dc584fc
= <=>
2021-01-26 15:51:46 +0100

    add

---
+++ js/exo2.js
@@ -0,0 +1,49 @@
+window.onload = function (){
+
+    // **Traduire en code ceci : faire une liste qui s'appelera panier,
+    // elle référencera à la fois le nom du légume ainsi que sont prix à l'unité**
+
+    var panier = [
+        {
+            Legume: 'carotte',
+            prixUnite: 2,
+            quantite : 3
+        },
+        {
+            Legume: 'tomate',
+            prixUnite: 1.40,
+            quantite : 2
+        },
+        {
+            Legume: 'petit pois',
+            prixUnite: 3.2,
+            quantite : 5
+        },
+        {
+            Legume: 'patate',
+            prixUnite: 1.90,
+            quantite : 6
+        }
+    ];
+
+    //**Traduire en ceci: crée une liste qui s'appelera prixTotalsFruitsLegumes cette liste devra contenir le total pour chaque fruit&légume. 
+    // Ce prix total sera variable par rapport à la quantité. (à noté que cette liste au début est vide).**
+   
+    var prixTotalsFruitsLegumes = [];
+
+     for(var Legume of panier){
+        var Total = Legume.prixUnite * Legume.quantite ;
+        prixTotalsFruitsLegumes.push(Total);
+     };
+     console.log("key :" ,prixTotalsFruitsLegumes , "value :", prixTotalsFruitsLegumes);
+
+     //Traduire en code ceci : crée un résultat permettant de faire le total de prixTotalsFruitsLegumes.
+
+     var prixTotal = 0;
+    for(var i= 0; i<prixTotalsFruitsLegumes.length ; i++){
+        prixTotal = prixTotal + prixTotalsFruitsLegumes[i] ;
+        //return prixTotal;
+    };
+    console.log(prixTotal);
+
+    }
--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 window.onload = function (){
+ //Traduire en code ceci : faire une liste 12 fruits et légumes qui s'appelera fruitsLegumes
 
     var fruitsLegumes = ["cerise","poire","banane", "raisins","poire","mirtille","papaye","goyage","mangue","poireau","celeri","tomate"]
     //console.log(fruitsLegumes);
 
-    var panier = [];
-    for( var item of fruitsLegumes){
-        //fruitsLegumes.shift(item);
-        //console.log("ajouté au panier:",panier[item]);
-         fruitsLegumes.shift();
-         //console.log(item)
-         panier.push(item)
-    }
-    console.log(panier)
-    console.log(fruitsLegumes)
-    
+    /*Traduire en code ceci: faire une liste qui référence tous ce que vous avez mis dans un panier, ce panier doit 
+être vide au départ.*/
 
+for(var item of fruitsLegumes ){
+    console.log("item tableau: ", item);
+    var fruitSuprime = fruitsLegumes.shift();
+    panier.push(fruitSuprime);
+
+};
+console.log("panier: ", panier);
+console.log(fruitsLegumes);
+
+for(var i=fruitsLegumes.length ; i>0 ; i--){
+    var fruitSuprime = fruitsLegumes.shift();
+    panier.push(fruitSuprime);
+ }
+ console.log("panier: ", panier);
+ console.log(fruitsLegumes);
 }
-
 
